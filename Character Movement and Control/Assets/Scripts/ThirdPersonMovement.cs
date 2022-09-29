b1112a8b44be6dd8dc88fed0b4c305a7cda2275c
Denise Tabilas <tabilas.denise@gmail.com>
2022-09-28 21:57:42 -0700

    fixed ThirdPersonMovement script
    there is gravity and the player can jump

--- Character Movement and Control/Assets/Scripts/ThirdPersonMovement.cs
+++ Character Movement and Control/Assets/Scripts/ThirdPersonMovement.cs
@@ -4,30 +4,53 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
+
 	public CharacterController controller;
 	public Transform cam; //create reference to camera 
 
 	public float speed = 6f;
 	public float turnSmoothTime = 0.1f;
+	float turnSmoothVelocity;
+
 
 	public float gravity = -9.81f;
+
 	public float jumpHeight = 3f;
 
+
 	public Transform groundCheck;
-	public float groundDistance = 0.4;
-	public LayerMask groundMask; 
+	public float groundDistance = 0.4f;
+	public LayerMask groundMask;
 
 	Vector3 velocity;
-	float turnSmoothVelocity;
 	bool isGrounded;
+
+	void Start()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
 
 	void Update()
 	{
+		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+		if (isGrounded && velocity.y < 0)
+		{
+			velocity.y = -2f; //reset velocity accordingly 
+		}
+
+
 		float horizontal = Input.GetAxisRaw("Horizontal");
 		float vertical = Input.GetAxisRaw("Vertical");
 		Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; //dont wanna move it on y axis 
-		velocity.y += gravity * Time.deltaTime;
 
+		if (Input.GetButtonDown("Jump") && isGrounded)
+		{
+			velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+		}
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move(velocity * Time.deltaTime);
 		if (direction.magnitude >= 0.1f)
 		{
 			float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -36,6 +59,9 @@ public class ThirdPersonMovement : MonoBehaviour
 
 			Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
 			controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+
+
 		}
 	}
 }
