b1bc8d5cf34ad9060f82fb39774c92546c7aa23f
Denise Tabilas <tabilas.denise@gmail.com>
2022-10-26 21:33:56 -0700

    created quest object for questing system

---
+++ Level Design/Assets/Scripts/Quest/Quest.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Quest", menuName="Inventory/Quest")] //create item in the project
+public class Quest : ScriptableObject
+{
+    /*
+    quest object will store 
+        title
+        description 
+        items to be collected 
+        reward   
+    */
+
+
+    new public string name = "New Quest";
+    public string description = "Description";
+    public List<Item> itemsToCollect = new List<Item>();
+    public List<Item> itemRewards = new List<Item>();
+     public virtual void Activate()
+     {
+          Debug.Log("Activating quest: " + name);
+     }
+}
