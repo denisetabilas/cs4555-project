54e033f2fd580bf56a5e874960160b6a84e7ffa0
Fabio Carrasco <fabio.carrasco97@gmail.com>
2022-11-10 14:43:47 -0800

    Activate Sword Item

--- Level Design/Assets/Scripts/Chest/TriggerAnimationItem.cs
+++ Level Design/Assets/Scripts/Chest/TriggerAnimationItem.cs
@@ -2,17 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerAnimation : MonoBehaviour
+public class TriggerAnimationItem : MonoBehaviour
 {
     [SerializeField] private Animator myChest;
     [SerializeField] private string Open = "Open";
+    [SerializeField] private GameObject item;
     private void OnTriggerEnter(Collider other)
     {
     if (other.CompareTag("Player"))
         {
             myChest.Play(Open, 0, 0.0f);
             gameObject.GetComponent<BoxCollider>().enabled = false;
-
+            item.SetActive(true);
         }
     }
 }
